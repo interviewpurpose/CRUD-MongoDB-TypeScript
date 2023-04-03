378576d62b414c00032abdba5f7aedc0959a891f
Selvi <reachyourgoal2006@gmail.com>
2023-04-03 21:10:59 +0530

    server created

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
+require('dotenv').config();
 import express, { Application, Request, Response } from "express";
+
 const app: Application = express();
-const port: number = 3000;
+
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("welcome");
 });
 
 app.listen(process.env.PORT, () => {
-  console.log(`server is running on port ${process.env.PORT}`);
-});
+    console.log(`server is running on port ${process.env.PORT}`)
+})
